02ecad19d6fe2da165fe114259aa61f88255f951
ThomasChin <140190333+ThomasChin@users.noreply.github.com>
2023-07-23 00:52:48 +0800

    Update script.js

--- script.js
+++ script.js
@@ -10,58 +10,67 @@ const firebaseConfig = {
   
   firebase.initializeApp(firebaseConfig);
   
-  let score = 0;
-  const leaderboardRef = firebase.database().ref("leaderboard");
-  const userNamesRef = firebase.database().ref("usernames"); // Reference for user names
-  
-  const scoreElement = document.getElementById("score");
-  const playerNameInput = document.getElementById("playerName");
-  const userNamesBox = document.getElementById("userNamesBox");
-  
-  function updateScore() {
-      scoreElement.textContent = score.toString();
-  }
-  
-  function updateLeaderboard() {
-      leaderboardRef.orderByChild("score").limitToLast(10).on("value", (snapshot) => {
-          const leaderboardElement = document.getElementById("leaderboard");
-          leaderboardElement.innerHTML = "";
-  
-          snapshot.forEach((childSnapshot) => {
-              const entry = childSnapshot.val();
-              const listItem = document.createElement("li");
-              listItem.textContent = `${entry.name} - ${entry.score}`;
-              leaderboardElement.appendChild(listItem);
-          });
-      });
-  }
-  
-  function handleClick() {
-      score += 1;
-      updateScore();
-  }
-  
-  const clickButton = document.getElementById("clickButton");
-  clickButton.addEventListener("click", handleClick);
-  
-  // Function to handle "Submit Name" button click event
-  function handleNameSubmit() {
-      const playerName = playerNameInput.value.trim();
-  
-      if (playerName !== "") {
-          playerNameInput.value = "";
-          userNamesRef.push({ name: playerName });
-  
-          // Add the submitted name to the userNamesBox
-          const listItem = document.createElement("li");
-          listItem.textContent = playerName;
-          userNamesBox.appendChild(listItem);
-      }
-  }
-  
-  const submitNameButton = document.getElementById("submitName");
-  submitNameButton.addEventListener("click", handleNameSubmit);
-  
-  // Call the updateLeaderboard function to initialize the leaderboard
-  updateLeaderboard();
+  firebase.initializeApp(firebaseConfig);
+
+let score = 0;
+const leaderboardRef = firebase.database().ref("leaderboard");
+const userNamesRef = firebase.database().ref("usernames"); // Reference for user names
+
+const scoreElement = document.getElementById("score");
+const playerNameInput = document.getElementById("playerName");
+const userNamesList = document.getElementById("userNamesList");
+
+function updateScore() {
+    scoreElement.textContent = score.toString();
+}
+
+function updateLeaderboard() {
+    leaderboardRef.orderByChild("score").limitToLast(10).on("value", (snapshot) => {
+        const leaderboardElement = document.getElementById("leaderboard");
+        leaderboardElement.innerHTML = "";
+
+        snapshot.forEach((childSnapshot) => {
+            const entry = childSnapshot.val();
+            const listItem = document.createElement("li");
+            listItem.textContent = `${entry.name} - ${entry.score}`;
+            leaderboardElement.appendChild(listItem);
+        });
+    });
+}
+
+function handleClick() {
+    score += 1;
+    updateScore();
+}
+
+const clickButton = document.getElementById("clickButton");
+clickButton.addEventListener("click", handleClick);
+
+// Function to handle "Submit Name" button click event
+function handleNameSubmit() {
+    const playerName = playerNameInput.value.trim();
+
+    if (playerName !== "") {
+        playerNameInput.value = "";
+        userNamesRef.push({ name: playerName });
+    }
+}
+
+const submitNameButton = document.getElementById("submitName");
+submitNameButton.addEventListener("click", handleNameSubmit);
+
+// Listen for changes in the user names list and update the display
+userNamesRef.on("value", (snapshot) => {
+    userNamesList.innerHTML = ""; // Clear the current list
+
+    snapshot.forEach((childSnapshot) => {
+        const entry = childSnapshot.val();
+        const listItem = document.createElement("li");
+        listItem.textContent = entry.name;
+        userNamesList.appendChild(listItem);
+    });
+});
+
+// Call the updateLeaderboard function to initialize the leaderboard
+updateLeaderboard();
   
